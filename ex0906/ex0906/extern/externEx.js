5beda1e63741199db9f3fd50fb14a5894af5d717
Shik Lee <isbread@daum.net>
2022-09-17 13:17:10 +0900

    first commit

---
+++ ex0906/ex0906/extern/externEx.js
@@ -0,0 +1,5 @@
+var age=10;
+//문자에 스타일 속성 적용
+document.write("<div style='color: red; font-size: 24px;'>외부 자바스크립트 파일</div>");
+document.write("당신의 나이는 "+age+"입니다.")
+// 외부 자바스크립트 파일에 작성시에는 <script>(스크립트태그)필요없음.
